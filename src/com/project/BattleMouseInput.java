f05696c6f1aa490cafc9b71e0745beffbdc43d37
Steve <stephen.northrop@hotmail.com>
2017-07-24 11:02:25 +0100

    bug fix

--- src/com/project/BattleMouseInput.java
+++ src/com/project/BattleMouseInput.java
@@ -6,7 +6,6 @@ public class BattleMouseInput extends MouseInput {
 	
 	public void mouseClicked(MouseEvent arg0) {
 		setpointClicked(arg0.getX(), arg0.getY());
-		UI.checkClick(arg0.getX(), arg0.getY());
 		System.out.println(Integer.toString(arg0.getX())+" "+Integer.toString(arg0.getY()));
 		BattleUI.checkButtons(arg0.getX(),arg0.getY());
 	}
