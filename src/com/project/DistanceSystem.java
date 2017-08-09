468dc8f69d53947e947d086e686a19e6351e9b81
Steve <stephen.northrop@hotmail.com>
2017-08-09 16:08:25 +0100

    distance meter

--- src/com/project/DistanceSystem.java
+++ src/com/project/DistanceSystem.java
@@ -10,16 +10,24 @@ public class DistanceSystem implements Handleable {
 	private static final int dotSize        = 10;
 	private static final int escapeDistance = 1000;
 	private static final double pixelRatio  = (double)lineLength/(double)escapeDistance;
+	private int distanceIncrement=1;
 	private int chaserSpeed=0;
 	private int chasedSpeed=0;
-	private int shipDistance = 500;
-	private int	chaserDist   = 75;
-	private int	chasedDist   = 75;
+	private int shipDistanceCurrent = 500;
+	private int shipDistanceDest = 500;
+	private int	chaserXCurrent   = 75;
+	private int	chasedXCurrent   = 75;
+	private int	chaserXDest;
+	private int	chasedXDest;
+	
 	
 	public DistanceSystem(int shipDistance, int chaserDist, int chasedDist) {
-		this.shipDistance = shipDistance;
-		this.chaserDist = (int) (chaserDist*pixelRatio);
-		this.chasedDist = (int) (chasedDist*pixelRatio);
+		this.shipDistanceDest = shipDistance;
+		this.shipDistanceCurrent = shipDistance;
+		this.chaserXCurrent = (int) (chaserDist*pixelRatio);
+		this.chasedXCurrent = (int) (chasedDist*pixelRatio);
+		this.chasedXDest    =  this.chasedXCurrent;
+		this.chaserXDest    =  this.chaserXCurrent;
 		Handler.addHighPriorityEntity(this);
 	}
 	public void calculateDistances(Ship chaser, Ship chased) {
@@ -41,12 +49,12 @@ public class DistanceSystem implements Handleable {
 		if(movingShip.getDistanceToEnd()==0 && movingShip.getSpeedChange()>0) {
 			int netSpeed = -(movingShip.getSpeed()-stationaryShip.getSpeed());
 			stationaryShip.increaseDistanceToEnd(netSpeed);
-			this.shipDistance-=netSpeed;
+			this.shipDistanceDest-=netSpeed;
 		}
 		else {
 			int netSpeed = movingShip.getSpeed()-stationaryShip.getSpeed();
 			movingShip.increaseDistanceToEnd(netSpeed);
-			this.shipDistance-=netSpeed;
+			this.shipDistanceDest-=netSpeed;
 		}
 		if(movingShip.getDistanceToEnd() < 0) {
 			stationaryShip.increaseDistanceToEnd(movingShip.getDistanceToEnd());
@@ -56,24 +64,47 @@ public class DistanceSystem implements Handleable {
 			movingShip.increaseDistanceToEnd(stationaryShip.getDistanceToEnd());
 			stationaryShip.increaseDistanceToEnd(-stationaryShip.getDistanceToEnd());
 		}
-		chaserDist = (int)(chaser.getDistanceToEnd()*pixelRatio);
-		chasedDist = (int)(chased.getDistanceToEnd()*pixelRatio);
+		chaserXDest = (int)(chaser.getDistanceToEnd()*pixelRatio);
+		chasedXDest = (int)(chased.getDistanceToEnd()*pixelRatio);
 		chaserSpeed = chaser.getSpeed();
 		chasedSpeed = chased.getSpeed();
+		int totalTicks;
+		if(Math.abs(chasedXDest-chasedXCurrent)>Math.abs(chaserXDest-chasedXCurrent)) {
+			  totalTicks = Math.abs(chasedXDest - chasedXCurrent);
+		}
+		else {
+			  totalTicks = Math.abs(chaserXDest - chaserXCurrent);
+		}
+		distanceIncrement = Math.abs(shipDistanceDest-shipDistanceCurrent)/totalTicks;
 
 	}
 	public void render(Graphics g) {
 		g.setColor(Color.WHITE);
 		g.drawLine(xLocation, yLocation, xLocation+lineLength, yLocation);
 		g.setColor(Color.WHITE);
-		g.drawString(String.valueOf(chaserSpeed),xLocation+chaserDist, yLocation-dotSize);
-		g.drawString(String.valueOf(chasedSpeed),xLocation+lineLength-chasedDist, yLocation-dotSize);
-		g.drawString(String.valueOf(shipDistance),xLocation+chaserDist+(int)(shipDistance*pixelRatio)/2, yLocation+dotSize+3);
-		g.fillOval(xLocation+chaserDist, yLocation-(dotSize/2), dotSize, dotSize);
-		g.fillOval(xLocation+lineLength-chasedDist, yLocation-(dotSize/2), dotSize, dotSize);
+		g.drawString(String.valueOf(chaserSpeed),xLocation+chaserXCurrent, yLocation-dotSize);
+		g.drawString(String.valueOf(chasedSpeed),xLocation+lineLength-chasedXCurrent, yLocation-dotSize);
+		g.drawString(String.valueOf(shipDistanceCurrent),xLocation+chaserXCurrent+(int)(shipDistanceDest*pixelRatio)/2, yLocation+dotSize+3);
+		g.fillOval(xLocation+chaserXCurrent, yLocation-(dotSize/2), dotSize, dotSize);
+		g.fillOval(xLocation+lineLength-chasedXCurrent, yLocation-(dotSize/2), dotSize, dotSize);
 
 	}
 	public void tick() {
-		
+		if(chasedXCurrent!=chasedXDest) {
+			int sign = chasedXDest>chasedXCurrent ? 1:-1; 
+			chasedXCurrent += sign;
+		}
+		if(chaserXCurrent!=chaserXDest) {
+			int sign = chaserXDest>chaserXCurrent ? 1:-1; 
+			chaserXCurrent += sign;
+		}
+		if(shipDistanceCurrent!=shipDistanceDest) {
+			int sign = shipDistanceDest>shipDistanceCurrent ? 1:-1; 
+			shipDistanceCurrent += distanceIncrement*sign;
+			if(shipDistanceCurrent>shipDistanceDest) {
+				shipDistanceCurrent=shipDistanceDest;
+			}
+
+		}
 	}
 }
--- src/com/project/battle/BattleKeyInput.java
+++ src/com/project/battle/BattleKeyInput.java
@@ -18,8 +18,15 @@ public class BattleKeyInput extends KeyInput {
 		if (key == KeyEvent.VK_Q){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(0));}
 		if (key == KeyEvent.VK_W){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(1));}
 		if (key == KeyEvent.VK_E){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(2));}
-		if (key == KeyEvent.VK_E){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(2));}
-		if (key == KeyEvent.VK_P){bs.setPaused(!bs.isPaused());}
+		if (key == KeyEvent.VK_R){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(3));}
+		if (key == KeyEvent.VK_T){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(4));}
+		if (key == KeyEvent.VK_Y){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(5));}
+		if (key == KeyEvent.VK_U){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(6));}
+		if (key == KeyEvent.VK_I){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(7));}
+		if (key == KeyEvent.VK_O){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(8));}
+		if (key == KeyEvent.VK_P){BattleUI.changeTootlipSelection(bs.playerShip.getCrew().get(9));}
+
+		//if (key == KeyEvent.VK_P){bs.setPaused(!bs.isPaused());}
 	}
 	@Override
 	public void keyReleased(KeyEvent e){
